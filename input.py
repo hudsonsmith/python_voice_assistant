79487b409c0c5a8a0b01b75c8a86e2c9eedf95d9
Pickaxe-7008 <fosa.srinand@gmail.com>
2024-06-23 12:40:08 -0500

    Create input.py
    detects wake word, takes in audio input and converts it to text.

---
+++ input.py
@@ -0,0 +1,50 @@
+import struct
+
+import speech_recognition as sr
+import pyaudio
+import pyttsx3
+import time
+import pvporcupine
+
+Active = False
+afkCounter = 0
+
+def speak(text, gender):
+     engine = pyttsx3.init('sapi5')
+     voices = engine.getProperty('voices')
+     if gender == "Female":
+         i = 1
+     else:
+         i = 0
+     engine.setProperty('voice',voices[i].id)
+     print("APOLLO:" + text + "\n")
+     engine.say(text)
+     engine.runAndWait()
+def getCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source=source)
+        try:
+            query = r.recognize_google(audio, language = "en-US")
+        except Exception as e:
+            query = ""
+    return query
+def runner(counter):
+    while True:
+        command = getCommand()
+        if command == "":
+            counter+= 1
+        print("User:" + command)
+        if "apollo" in command or "Apollo" in command:
+            speak("How may I help you?", "Male")
+            Active = True
+        elif "bye" in command:
+            speak("Going to sleep.", "Male")
+            Active = False
+        elif counter > 10 and Active == True:
+            speak("Going to sleep.", "Male")
+            Active = False
+
+
+
+runner(afkCounter)
