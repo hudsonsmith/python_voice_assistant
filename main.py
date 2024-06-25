350ec47a9f65444676e49e786c56a610bd3bcfa0
Pickaxe-7008 <fosa.srinand@gmail.com>
2024-06-25 11:15:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,53 @@
-from config import Config
-from src.bot import Bot
+import struct
 
-if __name__ == "__main__":
-    b: Bot = Bot(Config.name)
-    print(b)
-    b.run()
+import speech_recognition as sr
+import pyaudio
+import pyttsx3
+import time
+import pvporcupine
+
+Active = False
+afkCounter = 0
+
+def speak(text, gender):
+     engine = pyttsx3.init('sapi5')
+     voices = engine.getProperty('voices')
+     if gender == "Female":
+         i = 1
+     else:
+         i = 0
+     engine.setProperty('voice',voices[i].id)
+     print("APOLLO:" + text + "\n")
+     engine.say(text)
+     engine.runAndWait()
+def getCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source=source)
+        try:
+            query = r.recognize_google(audio, language = "en-US")
+        except Exception as e:
+            query = ""
+    return query.lower()
+
+def doTask():
+    print("Placeholder")
+def runner(afkCounter,Active):
+    while True:
+        command = getCommand()
+        print("User:" + command + "\n")
+        if Active:
+            doTask()
+        if "apollo" in command:
+            speak("How may I help you?", "Male")
+            Active = True
+        elif "bye" in command:
+            speak("Going to sleep.", "Male")
+            Active = False
+        elif afkCounter > 30 and Active == True:
+            speak("Going to sleep.", "Male")
+            Active = False
+
+
+
+runner(afkCounter,Active)
