958542513497951bd59bd10e7c1d2ca4b579848f
Pickaxe-7008 <fosa.srinand@gmail.com>
2024-07-19 16:22:38 -0500

    Update APOLLO.py

--- APOLLO.py
+++ APOLLO.py
@@ -7,10 +7,14 @@ import shutup
 import os
 import commands
 from commands import *
+import platform
+
+OperSys = platform.system()
+
 
 shutup.please()
 
-os.environ['OPENAI_API_KEY'] = "KEY HERE"
+os.environ['OPENAI_API_KEY'] = "SECRET KEY"
 def encode_image(image_path):
     with open(image_path, "rb") as image_file:
         return base64.b64encode(image_file.read()).decode('utf-8')
@@ -36,99 +40,13 @@ def outputCommands(command):
     screen_width = commands.screenWidth
 
     Planning_phase = f"""
-        You are APOLLO, a virtual personal assistant designed to assist, advise, and aid users based on their given commands.
-        You are familiar with the Windows operating system.
+        You are APOLLO, a personal virtual computer assistant developed by dAIlight Technologies designed to assist, advise, and aid users based on their given commands.
+        You are familiar with the {OperSys} operating system.
         You can see a computer screen with height: {screen_height}, width: {screen_width}, and the current task is "{command}". You need to return a plan to accomplish this goal.
         Please return the plan in the format of concise and essential subtasks in the form of subtasks.append("subtask details") statements in order. Avoid redundancy and minimize the number of subtasks. There is to be no outside commentary or explanation, only the code.
         You do not need to define the subtask list, it will be predefined. Use minimal subtasks, if it can be completed with just 1 use that (for example if the user wants you to tell them something based on information you already know).
-        Just so you know, you have the ability to speak to the user, and webscrape for information that you do not currently have.
+        Just so you know, you have the ability to speak to the user.
         You can also ask questions to the user as subtasks, in case you need specific information that your current knowledge base does not have, such as login credentials or specific inputs the user may want for a task such as the date of a flight ticket. In that same subtask you will also add that the memory needs to be appended with this information.        
-        Each subtask's description will be used to generate a python script later on to carry it out. Here are the functions that the python script will be allowed to use:
-        
-        def screenSpecs():
-                        return screenWidth, screenHeight, currentMouseX, currentMouseY
-        
-                    def moveCursor(x, y):
-                        pyautogui.moveTo(x, y)
-                    
-                    def rightClick():
-                        pyautogui.rightClick()
-                    
-                    def leftClick():
-                        pyautogui.leftClick()
-                    
-                    def write(text):
-                        pyautogui.write(text)
-                    
-                    def drag(x, y):
-                        pyautogui.dragTo(x, y)
-                    
-                    def press(key):
-                        pyautogui.press(key)
-                    
-                    def scroll(clicks):
-                        pyautogui.scroll(clicks)
-                    
-                    def wait(seconds):
-                        time.sleep(seconds)
-                    
-                    def subprocessTwo(lis):
-                        process = subprocess.Popen(lis, shell=True)
-                        process.wait()
-                    
-                    def keyDown(key):
-                        pyautogui.keyDown(key)
-                    
-                    def keyUp(key):
-                        pyautogui.keyUp(key)
-                    
-                    def hotkey(tup):
-                        pyautogui.hotkey(tup)
-                    
-                    def doubleClick():
-                        pyautogui.rightClick()
-                        pyautogui.rightClick()
-                    
-                    def open_application(app_name):
-                        pyautogui.press('win')
-                        pyautogui.write(app_name)
-                        pyautogui.press('enter')
-                    
-                    def minimize_window(window_name):
-                        myWindow = gw.getWindowsWithTitle(window_name)
-                        myWindow.minimize()
-                    
-                    def maximize_window(window_name):
-                        myWindow = gw.getWindowsWithTitle(window_name)
-                        myWindow.maximize()
-                    
-                    def resize_window(window_name,x,y):
-                        myWindow = gw.getWindowsWithTitle(window_name)
-                        myWindow.resizeTo(x,y)
-                    
-                    def use_searchBar(browser,link,query):
-                        if browser == "chrome":
-                            driver = webdriver.chrome()
-                        elif browser == "firefox":
-                            driver = webdriver.firefox()
-                        elif browser == "edge":
-                            driver = webdriver.edge
-                        driver.get(link)
-                        search_bar = driver.find_element(By.XPATH, '//*[@id="search"]')
-                        search_bar.send_keys(query)
-                        search_bar.submit()
-                    
-                    def web_scrape(link):
-                        r = requests.get(link)
-                        soup = BeautifulSoup(r.content, 'html.parser')
-                        s = soup.find('div', class_='entry-content')
-                        return (s.find_all('p'))
-                    
-                    def minimize_all_windows():
-                        subprocess.run('powershell -command "& (Add-Type -TypeDefinition \\"using System; using System.Runtime.InteropServices; public class Win32 ( [DllImport(\\"user32.dll\\")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow); )\\"; [System.Diagnostics.Process]::GetProcesses() | ForEach-Object ( $handle = $_.MainWindowHandle; [Win32]::ShowWindowAsync($handle, 6) ) )"',shell=True)                                 
-        
-        Reminder, your job is to generate the subtaks and their descrptions, not the code itself, this is just for your reference to see what your capabilities are. Do not returns the functions directly, you are to return written descriptions of each subtask that needs to be done in order.
-        
         Here is a screenshot of the existing screen state, reference your planning off of the current screen state as context if needed:
     """
     base64_image = encode_image(commands.screenshot())
@@ -170,19 +88,12 @@ def outputCommands(command):
 def reflect_task(APOLLOCommander, taskSub, plan_list, after_image):
     Reflection_phase = f"""
     Your goal was to complete the subtask: "{taskSub}". Here is the implementation plan you followed: {plan_list}.
-
     Check if the subtask "{taskSub}" has been completed correctly based on the screenshot that will be given at the bottom.
-
     If it was completed correctly, return True:NA.
-
     If it wasn't, return the answer in the format of False:insert reason for why you think it wasn't completed correctly and what you can do next time based on the user advice if it was given. 
-    
     In the reason also add what the code ended up actually doing, and how you can better it so it succeeds next attempt. If the entire plan needs to be changed, please detail the specifics of that as well.
-    
     There will be no extra commentary.
-    
     If the subtask involves speaking or taking user input, automatically mark it as True:NA.
-    
     Here is a screenshot of the current screen state after your code was executed, for reference:
     """
     APOLLOOut = APOLLOCommander.chat.completions.create(
@@ -216,99 +127,87 @@ def reflect_task(APOLLOCommander, taskSub, plan_list, after_image):
 
 def execute_task_with_feedback(taskSub, APOLLOCommander, screen_width, screen_height, plan_list, command,
                                memoryStorage):
+    dash = {"time": currTime(), "cpu": currCpu(), "ram": currRam(), "apps": currApps()}
     Latest_mistake = ""
     attempts = 0
     max_attempts = 3
     while attempts < max_attempts:
         Action_phase = f"""
-                    You are familiar with the Windows operating system and UI operations. Your goal now is to manipulate a computer screen, width: {screen_width}, height: {screen_height}, to complete the task: "{command}". Here is the implementation plan: {plan_list}. The current subtask is "{taskSub}".
-
+                    You are APOLLO, a personal virtual computer assistant developed by dAIlight Technologies, familiar with the {OperSys} operating system and UI operations. Your goal now is to manipulate a computer screen, width: {screen_width}, height: {screen_height}, to complete the task: "{command}". Here is the implementation plan: {plan_list}. The current subtask is "{taskSub}".
+                    
+                    Here is a dashboard with information about the computer enviornment: 
+                    
+                    Current time in the format of year, month, day, hour, minute, second, and microsecond: {dash['time']}
+                    
+                    Current cpu usage: {dash['cpu']}
+                    
+                    Current ram usage: {dash['ram']}
+                    
+                    Current apps open: {dash['apps']}
+                    
                     You can use the mouse and keyboard, along with other computer functions and terminal, using only the following functions:
                     
-                    def screenSpecs():
-                        return screenWidth, screenHeight, currentMouseX, currentMouseY
-        
-                    def moveCursor(x, y):
+                    -moveCursor(x, y):
+                        x = (x / 100) * screenWidth
+                        y = (y / 100) * screenHeight
                         pyautogui.moveTo(x, y)
                     
-                    def rightClick():
+                    -rightClick():
                         pyautogui.rightClick()
                     
-                    def leftClick():
+                    -leftClick():
                         pyautogui.leftClick()
                     
-                    def write(text):
+                    -write(text):
                         pyautogui.write(text)
                     
-                    def drag(x, y):
-                        pyautogui.dragTo(x, y)
+                    -drag(x,y):
+                        x = (x/100)*screenWidth
+                        y = (y/100)*screenHeight
+                        pyautogui.dragTo(x,y)
                     
-                    def press(key):
+                    -press(key):
                         pyautogui.press(key)
                     
-                    def scroll(clicks):
+                    -scroll(clicks):
                         pyautogui.scroll(clicks)
                     
-                    def wait(seconds):
+                    -wait(seconds):
                         time.sleep(seconds)
                     
-                    def subprocessTwo(lis):
+                    -subprocessTwo(lis):
                         process = subprocess.Popen(lis, shell=True)
                         process.wait()
                     
-                    def keyDown(key):
+                    -keyDown(key):
                         pyautogui.keyDown(key)
-                    
-                    def keyUp(key):
+                    -keyUp(key):
                         pyautogui.keyUp(key)
                     
-                    def hotkey(tup):
+                    -hotkey(tup):
                         pyautogui.hotkey(tup)
                     
-                    def doubleClick():
+                    -doubleClick():
                         pyautogui.rightClick()
                         pyautogui.rightClick()
                     
-                    def open_application(app_name):
+                    -open_application(app_name):
                         pyautogui.press('win')
                         pyautogui.write(app_name)
                         pyautogui.press('enter')
-                    
-                    def minimize_window(window_name):
-                        myWindow = gw.getWindowsWithTitle(window_name)
-                        myWindow.minimize()
-                    
-                    def maximize_window(window_name):
-                        myWindow = gw.getWindowsWithTitle(window_name)
-                        myWindow.maximize()
-                    
-                    def resize_window(window_name,x,y):
-                        myWindow = gw.getWindowsWithTitle(window_name)
-                        myWindow.resizeTo(x,y)
-                    
-                    def use_searchBar(browser,link,query):
-                        if browser == "chrome":
-                            driver = webdriver.chrome()
-                        elif browser == "firefox":
-                            driver = webdriver.firefox()
-                        elif browser == "edge":
-                            driver = webdriver.edge
-                        driver.get(link)
-                        search_bar = driver.find_element(By.XPATH, '//*[@id="search"]')
-                        search_bar.send_keys(query)
-                        search_bar.submit()
-                    
-                    def web_scrape(link):
-                        r = requests.get(link)
-                        soup = BeautifulSoup(r.content, 'html.parser')
-                        s = soup.find('div', class_='entry-content')
-                        return (s.find_all('p'))
-                    
-                    def minimize_all_windows():
-                        subprocess.run('powershell -command "& (Add-Type -TypeDefinition \\"using System; using System.Runtime.InteropServices; public class Win32 ( [DllImport(\\"user32.dll\\")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow); )\\"; [System.Diagnostics.Process]::GetProcesses() | ForEach-Object ( $handle = $_.MainWindowHandle; [Win32]::ShowWindowAsync($handle, 6) ) )"',shell=True)                                 
+                        
+                    -use_searchBar(query):
+                        pyautogui.keyDown("ctrl")
+                        pyautogui.keyDown("l")
+                        pyautogui.keyUp("l")
+                        pyautogui.keyUp("ctrl")
+                        pyautogui.write(query)
+                        pyautogui.press("enter")
+                        
+                    The mouse position is relative to the top-left corner of the screen, you should return a percent value for the x and y of mouse action that require them, for example to move the cursor to the center of the screen do moveCursor(50,50). Now, return the next actions to complete the subtask "{taskSub}". There is to be no outside commentary or explanation, only the code.
 
-                    The mouse position is relative to the top-left corner of the screen. Return the next actions to complete the subtask "{taskSub}". There is to be no outside commentary or explanation, only the code.
-
+                    You may not use any other imports, but you can use basic python code along with these functions, such as adding for loops.
+                    
                     If you want to say or tell something to the user, please use the function speak("whatever you want to say","Male"). speak is already defined, there is no need to redefine it. Be very conversational and descriptive in your speech. Your speech should be charactterized by the following characteristics: polite, witty, intelligent, efficient, loyal, calm, supportive, and articulate presence, characterized by formal manners, a sense of humor, vast knowledge, precision, protective instincts, composure, adaptability, and sophistication.
 
                     If you have failed to complete your task, here is the reasoning for your latest failure: {Latest_mistake}.If it is blank, that means you have not failed yet.
