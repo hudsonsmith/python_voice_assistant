459070a6fa478dc4aef96a3bf620db23d04e1ee4
Pickaxe-7008 <fosa.srinand@gmail.com>
2024-08-01 18:50:23 -0500

    Update commands.py

--- commands.py
+++ commands.py
@@ -1,144 +1,158 @@
-import time
-import subprocess
-import datetime as dt
-import psutil
-import pyttsx3
-import wmi
-import pyautogui
-from PIL import Image, ImageDraw, ImageFont
-import requests
-from bs4 import BeautifulSoup
-import speech_recognition as sr
+    import time
+    import subprocess
+    import datetime as dt
+    import psutil
+    import pyttsx3
+    import pyautogui
+    import speech_recognition as sr
+    import pytesseract
+    from PIL import Image, ImageDraw
+    import mss
+    import numpy as np
+
+    import APOLLO
+
+    pyautogui.FAILSAFE = False
+
+    screenWidth, screenHeight = pyautogui.size()
+    currentMouseX, currentMouseY = pyautogui.position()
 
 
+    def screenSpecs():
+        return screenWidth, screenHeight, currentMouseX, currentMouseY
+
+    def moveCursor(x, y):
+        pyautogui.moveTo(x, y)
+
+    def rightClick(x,y):
+        moveCursor(x,y)
+        pyautogui.rightClick()
+
+    def leftClick(x,y):
+        moveCursor(x, y)
+        pyautogui.leftClick()
+
+    def write(text):
+        pyautogui.write(text)
+
+    def drag(x,y):
+        pyautogui.dragTo(x,y)
+
+    def press(key):
+        pyautogui.press(key)
+
+    def scroll(clicks):
+        pyautogui.scroll(clicks)
+
+    def wait(seconds):
+        time.sleep(seconds)
+
+    def useSubprocess(lis):
+        process = subprocess.Popen(lis, shell=True)
+        process.wait()
+
+    def keyDown(key):
+        pyautogui.keyDown(key)
+    def keyUp(key):
+        pyautogui.keyUp(key)
+
+    def hotkey(tup):
+        pyautogui.hotkey(tup)
+
+    def doubleClick():
+        pyautogui.rightClick()
+        pyautogui.rightClick()
+    def open_application(app_name):
+        pyautogui.press('win')
+        time.sleep(3)
+        pyautogui.write(app_name)
+        time.sleep(3)
+        pyautogui.press('enter')
+        time.sleep(3)
+    def use_searchBar(query):
+        pyautogui.keyDown("ctrl")
+        pyautogui.keyDown("l")
+        pyautogui.keyUp("l")
+        pyautogui.keyUp("ctrl")
+        pyautogui.write(query + " ")
+        pyautogui.press("enter")
+    def screenshot():
+        image = pyautogui.screenshot()
+        image_path = "screenshotForApollo.png"
+        image.save(image_path)
+        return image_path
 
 
-screenWidth, screenHeight = pyautogui.size()
-currentMouseX, currentMouseY = pyautogui.position()
+    def speak(text, gender):
+        engine = pyttsx3.init('sapi5')
+        voices = engine.getProperty('voices')
+        if gender == "Female":
+            i = 1
+        else:
+            i = 0
+        engine.setProperty('voice', voices[i].id)
+        engine.say(text)
+        engine.runAndWait()
 
+    def askQuestion(query):
+        speak(query,"Male")
+        return getCommand()
 
-def screenSpecs():
-    return screenWidth, screenHeight, currentMouseX, currentMouseY
+    def getCommand():
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            audio = r.listen(source)
+            try:
+                query = r.recognize_google(audio, language="en-US")
+            except:
+                query = ""
+        return query.lower()
 
-def moveCursor(x, y):
-    x = (x / 100) * screenWidth
-    y = (y / 100) * screenHeight
-    pyautogui.moveTo(x, y)
+    def doNothing():
+        pass
 
-def rightClick():
-    pyautogui.rightClick()
+    def capture_screen():
+        with mss.mss() as sct:
+            screenshot = sct.grab(sct.monitors[0])
+            img = Image.frombytes('RGB', (screenshot.width, screenshot.height), screenshot.rgb)
+            return img
 
-def leftClick():
-    pyautogui.leftClick()
+    def preprocess_image(img):
+        img_gray = img.convert('L')
+        img_np = np.array(img_gray)
+        return img_np
 
-def write(text):
-    pyautogui.write(text)
+    def draw_boxes(img, boxes, data):
+        draw = ImageDraw.Draw(img)
+        ocr_list = []
+        for i, box in enumerate(boxes):
+            if data['text'][i] != "":
+                draw.rectangle(box, outline="red", width=2)
+                (x1, y1, x2, y2) = box
+                cx = (x1 + x2)/2
+                cy = (y1 + y2)/2
+                ocr_list.append([data['text'][i],(cx,cy)])
+        return img,ocr_list
 
-def drag(x,y):
-    x = (x/100)*screenWidth
-    y = (y/100)*screenHeight
-    pyautogui.dragTo(x,y)
+    def detect_text(img):
+        data = pytesseract.image_to_data(img, output_type=pytesseract.Output.DICT, lang='eng')
+        boxes = []
+        for i in range(len(data['text'])):
+            if int(data['conf'][i]) > 40:
+                (x, y, w, h) = (data['left'][i], data['top'][i], data['width'][i], data['height'][i])
+                boxes.append((x, y, x + w, y + h))
+        return boxes, data
 
-def press(key):
-    pyautogui.press(key)
+    def screenshotOcr():
+        screen_img = capture_screen()
+        preprocessed_img = preprocess_image(screen_img)
+        text_boxes, data = detect_text(preprocessed_img)
+        image, ocr_list = draw_boxes(screen_img, text_boxes, data)
+        print(ocr_list)
+        image_path = "screenshotForApollo.png"
+        image.save(image_path)
+        return image_path, ocr_list
 
-def scroll(clicks):
-    pyautogui.scroll(clicks)
-
-def wait(seconds):
-    time.sleep(seconds)
-
-def useSubprocess(lis):
-    process = subprocess.Popen(lis, shell=True)
-    process.wait()
-
-def keyDown(key):
-    pyautogui.keyDown(key)
-def keyUp(key):
-    pyautogui.keyUp(key)
-
-def hotkey(tup):
-    pyautogui.hotkey(tup)
-
-def doubleClick():
-    pyautogui.rightClick()
-    pyautogui.rightClick()
-
-def open_application(app_name):
-    pyautogui.press('win')
-    pyautogui.write(app_name)
-    time.sleep(5)
-    pyautogui.press('enter')
-    time.sleep(5)
-def use_searchBar(query):
-    pyautogui.keyDown("ctrl")
-    pyautogui.keyDown("l")
-    pyautogui.keyUp("l")
-    pyautogui.keyUp("ctrl")
-    pyautogui.write(query + " ")
-    pyautogui.press("enter")
-def screenshot():
-    image = pyautogui.screenshot()
-    image_path = "screenshotForApollo.png"
-    image.save(image_path)
-    return image_path
-
-
-def screenshot_with_grid():
-    image = pyautogui.screenshot()
-    draw = ImageDraw.Draw(image)
-
-    step_size = 60
-
-    for x in range(0, screenWidth, step_size):
-        line = ((x, 0), (x, screenHeight))
-        draw.line(line, fill=128)
-
-    for y in range(0, screenHeight, step_size):
-        line = ((0, y), (screenWidth, y))
-        draw.line(line, fill=128)
-
-    for x in range(0, screenWidth, step_size):
-        for y in range(0, screenHeight, step_size):
-            draw.text((x + 5, y + 5), f'({round(x / screenWidth * 100,1)}, {round(y / screenHeight * 100,1)})', fill=128)
-
-    image_path = "screenshotForApolloGridded.png"
-    image.save(image_path)
-    return image_path
-
-def currTime():
-    return dt.datetime.now()
-
-def currCpu():
-    return psutil.cpu_percent()
-
-def currRam():
-    return psutil.virtual_memory().percent
-
-
-
-def speak(text, gender):
-    engine = pyttsx3.init('sapi5')
-    voices = engine.getProperty('voices')
-    if gender == "Female":
-        i = 1
-    else:
-        i = 0
-    engine.setProperty('voice', voices[i].id)
-    engine.say(text)
-    engine.runAndWait()
-
-def askQuestion(query):
-    speak(query,"Male")
-    return getCommand()
-
-def getCommand():
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        audio = r.listen(source)
-        try:
-            query = r.recognize_google(audio, language="en-US")
-        except:
-            query = ""
-    return query.lower()
+    def taskDone():
+        APOLLO.taskDone = True
 
